2ccedbbc6253fde4294092709bd81e7cc188af91
Marcelo Cano <mecano@catamarca.edu.ar>
2023-08-11 20:16:02 -0300

    Initial commit

---
+++ src/guia2ejercicio3/Guia2Ejercicio3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia2ejercicio3;
+
+/**
+ *
+ * @author Monitor_Plano
+ */
+public class Guia2Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
